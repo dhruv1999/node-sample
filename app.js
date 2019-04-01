78e47906ed1b04a70c4cb10cb09d8c0ce80a06b1
dhruv1999 <dhruv.anita100@gmail.com>
2019-04-01 18:50:28 +0530

    Working Properly

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ var path = require("path");
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
-var server =app.listen(process.env.PORT || '5000',function(){
+var server =app.listen(process.env.PORT || '4000',function(){
     console.log('app is working')
 })
